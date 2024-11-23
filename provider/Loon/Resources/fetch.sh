ce0678a32e9a4a0d53be90158706eb3a41c3605c
bemarkt <bemarkthg@gmail.com>
2024-11-23 15:54:27 +0800

    feat(Loon): ✨ Update kl.txt

--- provider/Loon/Resources/fetch.sh
+++ provider/Loon/Resources/fetch.sh
@@ -2,6 +2,8 @@
 
 mkdir -p kelee
 
+curl -s https://raw.githubusercontent.com/luestr/ProxyResource/refs/heads/main/README.md | grep -oE 'https:\/\/kelee\.one\/[^?&]+\.plugin' >kl.txt
+
 while IFS= read -r url; do
     curl -A "$USER_AGENT" -o "kelee/$(basename "$url")" "$url"
 done <kl.txt
