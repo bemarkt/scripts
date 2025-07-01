501bdfbffc00292c6e83639ebc339d3e53f15aa0
bemarkt <bemarkthg@gmail.com>
2025-07-01 21:41:55 +0800

    ci(Loon): 💚 adapt file name suffix changes

--- provider/Loon/Resources/fetch.sh
+++ provider/Loon/Resources/fetch.sh
@@ -2,8 +2,19 @@
 
 mkdir -p kelee
 
-curl -s https://raw.githubusercontent.com/luestr/ProxyResource/refs/heads/main/README.md | grep -oE 'https:\/\/kelee\.one\/[^?&]+\.plugin' >kl.txt
+echo "Searching for plugin links from remote repository..."
 
-while IFS= read -r url; do
-    curl -A "$USER_AGENT" -o "kelee/$(basename "$url")" "$url"
-done <kl.txt
+curl -s https://raw.githubusercontent.com/luestr/ProxyResource/refs/heads/main/README.md |
+    grep -oE 'https://kelee\.one/[^?&]+\.lpx' |
+    while IFS= read -r url; do
+        if [ -z "$url" ]; then
+            continue
+        fi
+
+        filename=$(basename "$url")
+        new_filename="${filename%.lpx}.plugin"
+
+        curl -L -A "$USER_AGENT" -o "kelee/$new_filename" "$url"
+    done
+
+echo "All plugins have been downloaded and renamed."
